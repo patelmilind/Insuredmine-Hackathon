4fa1a0e3e8a8b2e280b9db7067abb6969e784c45
milind9 <milind@insuredmine.com>
2022-02-13 17:23:26 +0530

    code update

--- proposal/index.js
+++ proposal/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express');
-var {getClientProposals, create, getProposal, updateProposalStatus, sendProposals} = require('./proposal.controller');
+var {getClientProposals, create, getProposal, updateProposalStatus, sendProposals, extractFromPdf} = require('./proposal.controller');
 
 var router = express.Router();
 
@@ -10,6 +10,7 @@ router.get('/preview/:id', getProposal);
 router.get('/acceptedTrack/:id', updateProposalStatus);
 router.post('/', create);
 router.post('/send/:id', sendProposals);
+router.post('/extractFromPdf', extractFromPdf);
 // router.put('/:id', controller.upsert);
 // router.patch('/:id', controller.patch);
 // router.delete('/:id', controller.destroy);
--- proposal/proposal.controller.js
+++ proposal/proposal.controller.js
@@ -3,7 +3,8 @@
 const uniqid = require('uniqid');
 const fs = require('fs');
 const handlebars = require('handlebars');
-//  var agents = require('../agents.json')
+const request = require("request");
+const pdf = require('pdf-parse');
 
 let category = ['Home','Auto'];
 
@@ -52,8 +53,66 @@ function updateProposalStatus(req, res) {
 
 function sendProposals(req,res) {
     let proposalId = req.params.id;
+    let proposalData = savedProposals.filter(ele => ele._id == proposalId);
+    proposalData = proposalData[0];
+
+    const templatePath = __dirname + `/templates/home/${proposalData.template}.html`;
+    const html = fs.readFileSync(templatePath, 'utf8');
+    let template = handlebars.compile(html);
+    const finalTemplate = template({ insuranceType: proposalData.category,acceptUrl:`${ngrok}/api/proposal/acceptedTrack/${req.params.id}` });
+    console.log("finalTemplate --> ",finalTemplate);
     // let template
-    console.log("snding Proposals - "); 
+    let endPoint = 'https://api.nylas.com/send';
+    let nylas_token = "6OvmqyeqNQwRv2HvuPvXiOXaMVWzIQ"; //milind@insuredmine.com nylas token
+    let body = {"to":[{"email":`${proposalData.clientEmail}`,"name":`${proposalData.clientName}`}],"cc":[],"bcc":[],"body":`${finalTemplate}`,"subject":"Proposal for","file_ids":[],"tracking":{"links":true,"opens":true,"thread_replies":false,"payload":"Any string you want!!"}};
+
+    let options = {
+        method: 'POST',
+        url: endPoint,
+        headers: { authorization: nylas_token },
+        body: body,
+        json: true
+    };
+    console.log("snding Proposals - ");
+    request(options, async function (error, response, body) {
+        if (error) {
+            // callback({ "message": "Something went wrong, Please re-connect your account from setting!" }, null);
+            res.status(503).send({"message":"null","error":error});
+
+        } else if (response.statusCode != 200) {
+            console.log("Some error occured in sending email- from nylas " + JSON.stringify(response));
+            res.status(503).send({"message":"Something went wrong, Please re-connect your account from setting!"});
+            // callback({ "message": "Something went wrong in sending email, Please try after some time or contact support" }, null);
+            //res.status(503).send({"message":"Something went wrong, Please re-connect your account from setting!"});
+        }
+        else {
+            res.send(JSON.stringify(body));
+        }
+        
+    });
+}
+
+function extractFromPdf(req,res) {
+
+  let dataBuffer = fs.readFileSync(__dirname + '/auto-example.pdf');
+ 
+    pdf(dataBuffer).then(function(data) {
+    
+        // number of pages
+        console.log("number of pages --> ",data.numpages);
+        // number of rendered pages
+        console.log("rendered pages --> ",data.numrender);
+        // PDF info
+        console.log(" PDF info --> ",data.info);
+        // PDF metadata
+        console.log("PDF metadata -->", data.metadata); 
+        // PDF.js version
+        // check https://mozilla.github.io/pdf.js/getting_started/
+        // console.log(data.version);
+        // PDF text
+        console.log("PDF text --> ",data.text); 
+            
+    });
 }
 
 exports.getClientProposals = getClientProposals;
@@ -61,3 +120,4 @@ exports.create = create;
 exports.getProposal = getProposal;
 exports.updateProposalStatus = updateProposalStatus;
 exports.sendProposals = sendProposals;
+exports.extractFromPdf = extractFromPdf;
